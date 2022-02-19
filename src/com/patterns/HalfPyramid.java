1080dc0a1d589904b35f6e4c8123f79d9fbe5f59
suryakiran138 <suryakiransince1992@gmail.com>
2022-02-19 15:55:11 +0530

    added three pattern solutions

---
+++ src/com/patterns/HalfPyramid.java
@@ -0,0 +1,13 @@
+package com.patterns;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int m=5;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
