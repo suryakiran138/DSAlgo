e1631e006d57b28db593b367c674c946e136114c
suryakiran138 <suryakiransince1992@gmail.com>
2022-02-23 14:15:19 +0530

    changed implementation of insersion sort

--- src/com/sorting/InsertionSort.java
+++ src/com/sorting/InsertionSort.java
@@ -16,12 +16,12 @@ public class InsertionSort {
 
     public static void insertionSort(int[] arr) {
         for(int i=1;i<arr.length;i++){
-            for(int j=i;j>0;j--){
-                if(arr[j-1]>arr[j]){
-                    swap(arr,j-1,j);
-                }else
-                    break;
-            }
+           int current = arr[i];
+           int j= i-1;
+           for(;j>=0 && current<arr[j]; j--){
+                arr[j+1] = arr[j];
+           }
+           arr[j+1] = current;
         }
     }
 }
