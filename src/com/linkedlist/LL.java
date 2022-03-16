5ade944a9f16c9ccb465c2137d5ffa9e35c17e49
suryakiran138 <suryakiransince1992@gmail.com>
2022-03-16 19:09:35 +0530

    reverse linkist both iterative and recursive approaches

--- src/com/linkedlist/LL.java
+++ src/com/linkedlist/LL.java
@@ -2,6 +2,7 @@ package com.linkedlist;
 
 public class LL {
     Node head;
+    int size=0;
 
     public class Node {
         String data;
@@ -13,6 +14,7 @@ public class LL {
     }
 
     public void addFirst(String data){
+        size++;
         Node node = new Node(data);
         if(head == null){
             head = node;
@@ -24,6 +26,7 @@ public class LL {
     }
 
     public void addLast(String data){
+        size++;
         Node node = new Node(data);
         if(head == null){
             head = node;
@@ -54,6 +57,7 @@ public class LL {
         if(head == null) {
             System.out.println("Linked List is empty");
         }else {
+            size--;
             head = head.next;
         }
     }
@@ -62,14 +66,47 @@ public class LL {
         if(head == null) {
             System.out.println("Linked List is empty");
         }else if(head.next == null){
+            size--;
             head = null;
         }else {
             Node curr = head;
+            size--;
             while(curr.next.next != null){
                 curr = curr.next;
             }
             curr.next = null;
         }
+    }
+
+    public void reverseIterative() {
+        if(head == null && head.next == null){
+            return;
+        }
+        Node curr = head.next;
+        Node prev = head;
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head.next = null;
+        head = prev;
+    }
+
+    public void reverseRecursiveHelper(Node head){
+        if(head.next == null ){
+            this.head = head;
+            return;
+        }
+        reverseRecursiveHelper(head.next);
+        head.next.next = head;
+    }
+
+    public void reverseRecursive() {
+        Node origin = head;
+        reverseRecursiveHelper(head);
+        origin.next = null;
     }
 
     public static void main(String[] args) {
@@ -78,8 +115,10 @@ public class LL {
         linkedList.addFirst("This");
         linkedList.addLast("Linked List");
         linkedList.printList();
-        linkedList.deleteFirst();
-        linkedList.deleteLast();
+        System.out.println("size: "+linkedList.size);
+        linkedList.reverseIterative();
+        linkedList.printList();
+        linkedList.reverseRecursive();
         linkedList.printList();
     }
 }
