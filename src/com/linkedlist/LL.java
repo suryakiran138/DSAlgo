2323ac17a863a4fe80325f59a3f6c336677ef070
suryakiran138 <suryakiransince1992@gmail.com>
2022-03-16 14:14:43 +0530

    added delete methods to linked list

--- src/com/linkedlist/LL.java
+++ src/com/linkedlist/LL.java
@@ -50,11 +50,36 @@ public class LL {
         }
     }
 
+    public void deleteFirst(){
+        if(head == null) {
+            System.out.println("Linked List is empty");
+        }else {
+            head = head.next;
+        }
+    }
+
+    public void deleteLast(){
+        if(head == null) {
+            System.out.println("Linked List is empty");
+        }else if(head.next == null){
+            head = null;
+        }else {
+            Node curr = head;
+            while(curr.next.next != null){
+                curr = curr.next;
+            }
+            curr.next = null;
+        }
+    }
+
     public static void main(String[] args) {
         LL linkedList = new LL();
         linkedList.addFirst("is");
         linkedList.addFirst("This");
         linkedList.addLast("Linked List");
+        linkedList.printList();
+        linkedList.deleteFirst();
+        linkedList.deleteLast();
         linkedList.printList();
     }
 }
